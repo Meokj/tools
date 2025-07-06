3096874a9f2daeeb985348eac72fd13163a1d664
Meokj <153259973+Meokj@users.noreply.github.com>
2025-07-06 15:53:46 +0800

    Update install.sh

--- anyReality/install.sh
+++ anyReality/install.sh
@@ -37,7 +37,7 @@ if [ -d anytls ]; then
 fi
 
 rm -f sing-box-1.12.0-beta.28-linux-amd64.tar.gz
-wget https://github.com/SagerNet/sing-box/releases/download/v1.12.0-beta.28/sing-box-1.12.0-beta.28-linux-amd64.tar.gz
+wget https://github.com/SagerNet/sing-box/releases/download/v1.12.0-beta.28/sing-box-1.12.0-beta.30-linux-amd64.tar.gz
 tar -xzvf sing-box-1.12.0-beta.28-linux-amd64.tar.gz && \
 mv sing-box-1.12.0-beta.28-linux-amd64 anytls
 cd anytls || exit
