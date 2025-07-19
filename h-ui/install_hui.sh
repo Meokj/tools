288c36e97965ea34b92e8e7cc38482b711b5874c
Meokj <153259973+Meokj@users.noreply.github.com>
2025-07-19 23:26:52 +0800

    Update install_hui.sh

--- h-ui/install_hui.sh
+++ h-ui/install_hui.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 clear
+IP=$(hostname -I | awk '{print $1}')
+
+read -p "echo "请输入自定义面板端口号（1024-65535）：" PORT
+if [[ "$PORT" -ge 1024 && "$PORT" -le 65535 ]]; then
+    if netstat -tuln | grep -q ":$PORT"; then
+        echo "端口 $PORT 已被占用"
+        exit 1
+    fi
+else
+    echo "无效的端口号，请输入范围在1024到65535之间的数字"
+    exit 1
+fi
+
 mkdir -p /usr/local/h-ui/
 curl -fsSL https://github.com/jonssonyan/h-ui/releases/latest/download/h-ui-linux-amd64 -o /usr/local/h-ui/h-ui && chmod +x /usr/local/h-ui/h-ui
 curl -fsSL https://raw.githubusercontent.com/jonssonyan/h-ui/main/h-ui.service -o /etc/systemd/system/h-ui.service
-sed -i "s|^ExecStart=.*|ExecStart=/usr/local/h-ui/h-ui -p 6812|" "/etc/systemd/system/h-ui.service"
+sed -i "s|^ExecStart=.*|ExecStart=/usr/local/h-ui/h-ui -p $PORT|" "/etc/systemd/system/h-ui.service"
 systemctl daemon-reload
 systemctl enable h-ui
 systemctl restart h-ui
@@ -24,8 +37,7 @@ sudo timedatectl set-timezone Asia/Shanghai
 (crontab -l 2>/dev/null; echo "0 4 * * * $RESTART_HUI") | crontab -
 echo "h-ui服务安装完成，定时任务已设置为每天凌晨4点重启服务!!!"
 echo
-echo "登录方式：IP:PORT"
-echo "面板端口：6812"
+echo "登录地址：$IP:$PORT"
 echo "用户名：sysadmin"
 echo "密码：sysadmin"
 echo
